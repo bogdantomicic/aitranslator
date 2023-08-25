49e73c7be406dd672d60c438ebdd570a8f417c08
Bogdan <bogdantomicic@gmail.com>
2023-08-25 09:33:16 +0200

    premjestio voice recording u svoju componentu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import wordList from './englishWords.json';
 import './App.css';
 import axios from 'axios';
+import {VoiceRecording}  from "./components/VoiceRecording";
 
 const GOOGLE_API_KEY = "AIzaSyCDSKkD5pZl7j40eIs2Tk5LzAV6vboXqZU";
 const API_KEY = "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";
@@ -19,9 +20,7 @@ function App() {
   const [inputWord, setInputWord] = useState('');
   const [translatedWord, setTranslatedWord] = useState('');
 
-  const [isRecording, setIsRecording] = useState(false);
-  const [transcript, setTranscript] = useState('');
-  const [audioChunks, setAudioChunks] = useState([]);
+
 
   const levels = ['A0', 'A1', 'B1', 'B1+', 'C1', 'C2'];
   const handleLevelChange = (event) => {
@@ -44,62 +43,7 @@ function App() {
 
   ////
 
-  let mediaRecorder = null;
-
-  const handleDataAvailable = (event) => {
-    if (event.data.size > 0) {
-      setAudioChunks((chunks) => [...chunks, event.data]);
-    }
-  };
-
-  const startRecording = () => {
-    setIsRecording(true);
-    setTranscript('');
-    setAudioChunks([]);
-
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then((stream) => {
-        mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.ondataavailable = handleDataAvailable;
-        mediaRecorder.start();
-      })
-      .catch((error) => {
-        console.error('Error accessing microphone:', error);
-        setIsRecording(false);
-      });
-  };
-
-  const stopRecording = async () => {
-    setIsRecording(false);
-
-    if (mediaRecorder) {
-      mediaRecorder.stop();
-    }
-
-    // Convert and send audio to Whisper API
-    const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
-    const apiKey = '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';
-
-    try {
-      const formData = new FormData();
-      formData.append('file', audioBlob);
-
-      const response = await axios.post(
-        'http://localhost:4000/whisper/asr',  // Koristi svoju IP adresu ili domenu
-        formData,
-        {
-          headers: {
-            Authorization: `Bearer ${apiKey}`,
-          },
-        }
-      );
-      
-
-      // Handle response and set transcript state
-      setTranscript(response.data.text);
-    } catch (error) {
-      console.error('Error:', error);
-    }}
+  
 
   ////
 
@@ -299,17 +243,7 @@ function App() {
         ) : null}
       </div>
 
-      <div className='bg-white'>
-      {isRecording ? (
-        <button onClick={stopRecording}>Stop Recording</button>
-      ) : (
-        <button onClick={startRecording}>Start Recording</button>
-      )}
-      <div>
-        <p className='text-black'>Transcript: {transcript}</p>
-      </div>
-    </div>
-
+      <VoiceRecording></VoiceRecording>
     </div>
   );
 }
---
+++ src/components/VoiceRecording.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import axios from 'axios';
+
+
+export function VoiceRecording() {
+
+    const [isRecording, setIsRecording] = useState(false);
+    const [transcript, setTranscript] = useState('');
+    const [audioChunks, setAudioChunks] = useState([]);
+
+let mediaRecorder = null;
+
+  const handleDataAvailable = (event) => {
+    if (event.data.size > 0) {
+      setAudioChunks((chunks) => [...chunks, event.data]);
+    }
+  };
+
+  const startRecording = () => {
+    setIsRecording(true);
+    setTranscript('');
+    setAudioChunks([]);
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then((stream) => {
+        mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.ondataavailable = handleDataAvailable;
+        mediaRecorder.start();
+      })
+      .catch((error) => {
+        console.error('Error accessing microphone:', error);
+        setIsRecording(false);
+      });
+  };
+
+  const stopRecording = async () => {
+    setIsRecording(false);
+
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+    }
+
+    // Convert and send audio to Whisper API
+    const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+    const apiKey = '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';
+
+    try {
+      const formData = new FormData();
+      formData.append('file', audioBlob);
+
+      const response = await axios.post(
+        'http://localhost:4000/whisper/asr',  // Koristi svoju IP adresu ili domenu
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
+      
+
+      // Handle response and set transcript state
+      setTranscript(response.data.text);
+    } catch (error) {
+      console.error('Error:', error);
+    }}
+
+    return (
+        <div className='bg-white'>
+      {isRecording ? (
+        <button onClick={stopRecording}>Stop Recording</button>
+      ) : (
+        <button onClick={startRecording}>Start Recording</button>
+      )}
+      <div>
+        <p className='text-black'>Transcript: {transcript}</p>
+      </div>
+    </div>
+    )
+
+}
+
